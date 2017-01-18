dfea498c118d3147484f25db9c11d0eb593e3499
andrea_cajamarca <andreakcajamarca@gmail.com>
2017-01-18 23:23:58 +0000

    added functions into js

--- website/script.js
+++ website/script.js
@@ -1,3 +1,16 @@
-/// http://api.openweathermap.org/data/2.5/weather?q=miami&units=imperial&appid=384b90ab8ec4f24f794710df80764a4a
+/// 
 ///Keep this in mind!
-
+    $(document).ready(function() {
+    
+   get.JSON(
+       "http://api.openweathermap.org/data/2.5/weather?q=miami&units=imperial&appid=384b90ab8ec4f24f794710df80764a4a"
+      
+       function(response) {
+        console.log(response);
+        $("body").append("<img src=" + response.data.visibility.wind.speed.deg.gust + ">");
+      });
+      
+     function info {
+         
+     }
+});
